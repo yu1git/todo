a771bf5a385e3c15fa69b90449e5eb8d58bb3dd5
yu1git <mitateyuu@gmail.com>
2021-11-30 10:42:33 +0900

    memo

--- memo/app/Http/Controllers/TaskController.php
+++ memo/app/Http/Controllers/TaskController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+        ]);
+    }
 }
