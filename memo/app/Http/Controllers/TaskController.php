6e066d5abaa72dc4496a06e65de23753cda3131a
yu1git <mitateyuu@gmail.com>
2021-12-08 15:17:25 +0900

    todo

--- memo/app/Http/Controllers/TaskController.php
+++ memo/app/Http/Controllers/TaskController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        //すべてのフォルダを取得する
         $folders = Folder::all();
+        
+        //選ばれたフォルダを取得する
+        //find メソッドは、プライマリキーのカラムを条件として一行分のデータを取得
+        $current_folder = Folder::find($id);
 
+        //選ばれたフォルダに紐づくタスクを取得する
+        //クエリビルダのwhere(カラム名,'=',比較する値)
+        //以下は「＝」を省略している。第二引数を変えると、イコール以外の比較も可能
+        //Tasks::where('folder_id', '=', $current_folder->id)->get();
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
--- memo/app/Task.php
+++ memo/app/Task.php
@@ -6,5 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * 状態定義
+     */
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    /**
+     * 状態のラベル
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+    // 状態値
+    $status = $this->attributes['status'];
+
+    // 定義されていなければ空文字を返す
+    if (!isset(self::STATUS[$status])){
+        return '';
+    }
+
+    return self::STATUS[$status]['label'];
+    }
+    
+    /**
+     * 状態を表すHTMLクラス
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
 }
